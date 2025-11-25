f3b9de25e45d089a19392a3c9a8da7c8bd6455ee
ArshiaAhmadipour <diniodduck@gmail.com>
2025-11-25 12:15:42 +0330

    add block, pic and nav

--- movies/views.py
+++ movies/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'movies/index.html', {})
+    context = {
+        'movies': ['gladiator', 'top gun', '007']
+    }
+    return render(request, 'movies/index.html', context)
 
 def about(request):
     return render(request, 'movies/about.html', {})
