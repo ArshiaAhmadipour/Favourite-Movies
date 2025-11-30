3ac89ff29c9b4e3130812eea3631ca87008db113
ArshiaAhmadipour <diniodduck@gmail.com>
2025-11-30 22:37:11 +0330

    add .html files

--- movies/views.py
+++ movies/views.py
@@ -1,11 +1,9 @@
 from django.shortcuts import render
+from .models import MediaItem
 
-# Create your views here.
 def index(request):
-    context = {
-        'movies': ['gladiator', 'top gun', '007']
-    }
-    return render(request, 'movies/index.html', context)
+    all_media = MediaItem.objects.exclude(rating__isnull=True).order_by('-rating', 'title')
+    return render(request, 'movies/index.html', {'all_media': all_media})
 
 def about(request):
-    return render(request, 'movies/about.html', {})
+    return render(request, 'movies/about.html')
